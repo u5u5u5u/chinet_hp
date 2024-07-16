20d4e8495ca6389544f354078125421bf59344fa
y-u-uki <takahashi.yuuki900@mail.kyutech.jp>
2024-07-16 19:08:23 +0900

    [add]色変えた#9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import styles from "./page.module.scss";
-
+import News from "@/components/News/News";
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <News />
+    </main>
+  );
 }
---
+++ src/components/News/News.tsx
@@ -0,0 +1,5 @@
+import styles from "./News.module.scss";
+const News = () => {
+  return <h1 className={styles.title}>news</h1>;
+};
+export default News;
