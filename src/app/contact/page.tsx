5760d2aaaaf3951da3cf894d35679dbea0eccbb6
kyuuu67 <koizumi.seigo470@mail.kyutech.jp>
2024-08-27 00:25:11 +0900

    必要な記入欄を作成しました。

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,10 +3,23 @@ import Input from "@/components/Inputs/Input"
 
 export default function Contact () {
     return (
-        <>
-        <h1 className={styles.title}>contact</h1>
+        <div className={styles.contactPage}>
+        
+        <h1 className={styles.title}>お問い合わせ</h1>
+        <p className={styles.description}>弊団体へのお問い合わせはこちらよりお願い致します。</p>
         <Input label="お名前"></Input>
         <Input label="会社/学校名"></Input>
-        </>
+        <Input label="メールアドレス"></Input>
+        <Input label="電話番号"></Input>
+        <div className={styles.textareaContainer}>
+        <label className={styles.label}>お問い合わせ内容</label>
+        <textarea className={styles.textareaField}></textarea>
+        </div>
+        <div className={styles.buttonContainer}>
+        <button className={styles.submitButton}>送信</button>
+        </div>
+        </div>
+        
+    
     );
 }
--- src/components/Inputs/Input.tsx
+++ src/components/Inputs/Input.tsx
@@ -9,9 +9,10 @@ type Props = {
 const Input = ({label}: Props) => {
     return (
         <div>
-            <label htmlFor="">{label}</label>
-            <input type="text" />
+            <label className={styles.label} htmlFor="">{label}</label>
+            <input className={styles.inputField} type="text" />
         </div>
     )
 }
 export default Input;
+
