388df428e306f72b25f39578726130be83931092
U5 <kyutech.joko@gmail.com>
2024-10-28 23:52:43 +0900

    basic認証適用#36

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
 
-export function middleware(request: NextRequest) {
-  console.log("middleware: ", request.url);
+export const middleware = createNextAuthMiddleware();
 
-  return NextResponse.next();
-}
-
-export const config = {};
+export const config = {
+  matcher: ["/(.*)"],
+};
