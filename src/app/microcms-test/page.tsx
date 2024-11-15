802406f96b692f79ce178cccecec03fec9b2f300
U5 <kyutech.joko@gmail.com>
2024-11-15 21:27:56 +0900

    トップページの記事数上限を5に変更#42

--- src/app/microcms-test/page.tsx
+++ src/app/microcms-test/page.tsx
@@ -6,6 +6,7 @@ const microcmsTest = async () => {
   const data = await getArticlesList({
     limit: TOP_ARTICLE_LIMIT,
   });
+
   return (
     <>
       <h1>MicroCMS Test</h1>
--- src/components/ArticleListTest/index.tsx
+++ src/components/ArticleListTest/index.tsx
@@ -9,6 +9,7 @@ const ArticleListTest = ({ articles }: Props) => {
   if (articles.length === 0) {
     return <p>記事がありません</p>;
   }
+
   return (
     <ul>
       {articles.map((article) => (
