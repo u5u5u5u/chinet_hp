fae9b2e1f3cda198066000cb3d4d5e0aa2eee410
U5 <kyutech.joko@gmail.com>
2025-02-18 19:18:35 +0900

    add:not-foundページ#51

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+const NotFound = () => {
+  return (
+    <div>
+      <dl>
+        <dt>ページが見つかりませんでした</dt>
+        <dd>
+          あなたがアクセスしようとしたページは存在しません。
+          <br />
+          URLを再度ご確認ください。
+        </dd>
+      </dl>
+    </div>
+  );
+};
+
+export default NotFound;
