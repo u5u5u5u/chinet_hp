4148e03943edc4ef9d2c7c724b0145fe48eb85fb
y-u-uki <takahashi.yuuki900@mail.kyutech.jp>
2025-03-08 11:19:24 +0900

    [fix]Linkに変更

--- src/components/News/index.tsx
+++ src/components/News/index.tsx
@@ -1,4 +1,3 @@
-"use client"
 import React from "react";
 import Image from "next/image";
 import {
@@ -11,6 +10,7 @@ import {
 import { getArticlesList } from "@/app/_libs/microcms";
 import { TOP_ARTICLE_LIMIT } from "@/app/_constants";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 export default async function NewsSection() {
   const data = await getArticlesList({
@@ -24,33 +24,32 @@ export default async function NewsSection() {
         <Carousel className="w-full max-w-5xl">
           <CarouselContent className="flex gap-4">
             {data.contents.map((content, index) => (
-              <CarouselItem key={index} className="basis-1/3">
-                <Card
-                  className="w-[320px] bg-white rounded-lg shadow-md overflow-hidden cursor-pointer"
-                  onClick={() => (window.location.href = content.thumbnail.url)}
-                >
-                  {content.thumbnail?.url && (
-                    <Image
-                      src={content.thumbnail.url}
-                      alt={content.title}
-                      width={320}
-                      height={240}
-                      className="w-full h-auto"
-                    />
-                  )}
-                  <CardHeader className="p-3">
-                    <p className="text-xs text-gray-500">
-                      {content.publishedAt}
-                    </p>
-                    <span className="text-xs bg-gray-200 text-gray-700 px-2 py-1 rounded-md">
-                      News
-                    </span>
-                    <CardTitle className="text-sm mt-1">
-                      {content.title}
-                    </CardTitle>
-                  </CardHeader>
-                </Card>
-              </CarouselItem>
+              <Link href={`/news/${content.id}`} key={index}>
+                <CarouselItem key={index} className="basis-1/3">
+                  <Card className="w-[320px] bg-white rounded-lg shadow-md overflow-hidden cursor-pointer">
+                    {content.thumbnail?.url && (
+                      <Image
+                        src={content.thumbnail.url}
+                        alt={content.title}
+                        width={320}
+                        height={240}
+                        className="w-full h-auto"
+                      />
+                    )}
+                    <CardHeader className="p-3">
+                      <p className="text-xs text-gray-500">
+                        {content.publishedAt}
+                      </p>
+                      <span className="text-xs bg-gray-200 text-gray-700 px-2 py-1 rounded-md">
+                        News
+                      </span>
+                      <CardTitle className="text-sm mt-1">
+                        {content.title}
+                      </CardTitle>
+                    </CardHeader>
+                  </Card>
+                </CarouselItem>
+              </Link>
             ))}
           </CarouselContent>
           <CarouselPrevious />
