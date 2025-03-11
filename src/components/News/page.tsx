79dbfe0af595db85e447018d4db0f7aa78c6e870
y-u-uki <takahashi.yuuki900@mail.kyutech.jp>
2025-03-12 07:37:27 +0900

    importをふやした

--- src/components/News/page.tsx
+++ src/components/News/page.tsx
@@ -1,8 +1,9 @@
 import { getArticlesList } from "@/app/_libs/microcms";
-import NewsSection from "@/components/News"; // クライアントコンポーネントをインポート
+import NewsSection from "@/components/News";
+import { TOP_ARTICLE_LIMIT } from "@/app/_constants";
 
 export default async function Page() {
   const data = await getArticlesList({ limit: TOP_ARTICLE_LIMIT });
 
-  return <NewsSection data={data} />; // クライアントコンポーネントにデータを渡す
+  return <NewsSection data={data} />;
 }
