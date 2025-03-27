4401930f2faef515c2b3197eb61ecd18af8c4011
U5 <kyutech.joko@gmail.com>
2025-03-27 22:01:46 +0900

    update:団体情報のデザイン修正#68

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -3,7 +3,7 @@ import { InformationContents } from "./contents";
 
 const Information = () => {
   return (
-    <Card className="flex justify-center items-center w-[60%] pt-8 pb-14 px-40 mx-auto my-20 bg-white/30 shadow-sm">
+    <Card className="flex justify-center items-center w-[80%] max-w-[700px] pt-8 pb-14 px-40 mx-auto my-20 bg-white/30 shadow-sm">
       <ul className="w-full text-foreground">
         {InformationContents.map((content, index) => (
           <li key={index} className="flex justify-between items-center border-b border-gray-300 py-8">
