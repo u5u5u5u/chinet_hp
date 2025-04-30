86c0df60cc280f8946db336fc7487153d8d07a24
U5 <kyutech.joko@gmail.com>
2025-04-30 11:08:48 +0900

    add:スクロールアニメーション実装#133

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,27 @@ import Greeting from "@/components/Greeting";
 import Information from "@/components/Information";
 import MainVisual from "@/components/MainVisual";
 import News from "@/components/News";
+import InView from "@/components/InView";
 
 export default function Home() {
   return (
     <main>
       <MainVisual />
-      <About />
-      <Activities />
-      <News />
-      <Greeting />
-      <Information />
+      <InView>
+        <About />
+      </InView>
+      <InView>
+        <Activities />
+      </InView>
+      <InView>
+        <News />
+      </InView>
+      <InView>
+        <Greeting />
+      </InView>
+      <InView>
+        <Information />
+      </InView>
     </main>
   );
 }
