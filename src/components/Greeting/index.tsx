393a1e34e2e53acf9baddf16c4fd9fba672fe4bc
asi3946 <yuuma218eldol@gmail.com>
2026-07-03 12:03:13 +0900

    [update]代表者名を縦並べに変更#163

--- src/components/Greeting/index.tsx
+++ src/components/Greeting/index.tsx
@@ -33,8 +33,15 @@ const Greeting = () => {
                 </p>
               )
             )}
-            <p className="text-right mt-4">
-              <span className="mr-4">代表</span>{REP.join("　")}</p>
+            <div className="mt-4 flex justify-end text-right">
+              <span className="mr-4">代表</span>
+                <div className="text-left">
+                  {REP.map((name, ndx) => (
+                    // biome-ignore lint/suspicious/noArrayIndexKey: 静的なテキスト配列のためインデックスで安全
+                    <div key={ndx}>{name}</div>
+                  ))}
+                </div>
+            </div>
           </div>
         </div>
       </div>
